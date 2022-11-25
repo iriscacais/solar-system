911285537b23356a43911dae096ec4df73cd78ec
iriscacais <mandeparairis@gmail.com>
2022-11-25 20:21:37 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import './App.css';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <SolarSystem />
+      </div>
     );
   }
 }
